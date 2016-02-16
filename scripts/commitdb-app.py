71aba112fccca748de74db8be076a3f8c93115c0
brichbourg <brichbourg@gmail.com>
2016-02-16 10:36:04 -0500

    Minor change

--- scripts/commitdb-app.py
+++ scripts/commitdb-app.py
@@ -74,22 +74,5 @@ try:
 	'''
 
 except:
-	#Start HTML print out, headers are printed so the Apache server on APP does not produce a malformed header 500 server error
-	print '''
-	<Content-type: text/html\\n\\n>
-	<html>
-	<head>
-	<title>Multi-Tier Web App</title>
-	</head>
-	<body>
-	<table border="1">
-	'''
-
 	appsitefunctions.printdbservererror()
 
-	#Finish printing headers 
-	print '''
-	</table>
-	</body>
-	</html>
-	'''
