1598054dc5eac516b807dece847f27c40f398887
Brantley Richbourg <brichbourg@gmail.com>
2016-09-02 09:29:56 -0400

    Updated install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 echo -e "We are going to update the site with the latest code from GitHub, press ENTER key to continue, or CTRL-C to quit"
 read key
 sudo cd ~/Multi-Tier-App-Demo
-sudo git pull 
+sudo mkdir /etc/mtwa
+sudo cp mtwa.conf /etc/mtwa/mtwa.conf
 sudo cp html/* /var/www/html/appdemo/
 sudo cp scripts/* /var/www/html/appdemo/
 
