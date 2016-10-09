10fb88cbd4f5e0adb87f56eca690421f8ee57bd6
Brantley Richbourg <brichbourg@gmail.com>
2016-10-09 13:29:39 -0400

    Fixed install.sh
    There was a missing mkdir command from the install.sh script.

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ echo -e "We are going to update the site with the latest code from GitHub, press
 read key
 sudo cd ~/Multi-Tier-App-Demo
 sudo mkdir /etc/mtwa
+sudo mkdir /var/www/html/appdemo
 sudo cp mtwa.conf /etc/mtwa/mtwa.conf
 sudo cp html/* /var/www/html/appdemo/
 sudo cp scripts/* /var/www/html/appdemo/
